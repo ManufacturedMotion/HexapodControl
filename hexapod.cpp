817db933725dc8028f16036f33a7bee7e2c707a7
zschwid <zschwid@gmail.com>
2024-02-27 22:16:45 -0500

    First pass of ik for entire bot

--- hexapod.cpp
+++ hexapod.cpp
@@ -39,4 +39,40 @@ void Hexapod::stand() {
 _Bool Hexapod::linearMove(Position next_pos) {
     return true;
 }
- 
+
+uint8_t Hexapod::inverse_kinematics(Position pos) {
+	// Just to get something workign assume yaw = 0 
+	// Must rework leg IK or set points align all coordinate systems
+	// Right now we have 6 coordinate systems harder than we want to do
+	// Even if we did like 2 coordinate systems (left / right) would be a big help for this
+	// Discuss with Danny/Dillon best way to accomplish this as there are many confusing effects of either way 
+
+	if (!preCheckSafePos(Position pos))
+        return 254; // Pre-check fail
+	
+	// trig stuff is unique to each leg to can't use loop efficientlys
+	for (uint8_t i = 0; i < NUM_LEGS; i++) {
+		_potential_results[i][0] = pos.X; // + trig stuff with yaw
+		_potential_results[i][1] = pos.Y; // + trig stuff with yaw
+		_potential_results[i][2] = pos.Z + sin(pos.pitch) * _leg_X_offset[i] + sin(pos.roll) * _leg_Y_offset[i]
+	}
+
+	if (!postCheckSafeCoords(_potential_resutls))
+		return 255; // Post-check fail
+
+	for (uint8_t i = 0; i < NUM_LEGS; i++) {
+		for (uint8_t j = 0; j < NUM_AXES_PER_LEG; j++) {
+			_next_leg_pos[i][j] = potential_results[i][j]
+		}
+	}
+}
+
+_Bool Hexapod::preCheckSafePos(Position pos) {
+	// this function might be unnecessary but keeping as a placeholder
+	return true;
+}
+
+_Bool Hexapod::postCheckSafeCoords(double x, double y, double x) {
+	// this function should check if legs will be in impossible positions or within the bot
+	return true;
+}
--- hexapod.hpp
+++ hexapod.hpp
@@ -20,6 +20,12 @@
 		private:
 			uint8_t _leg_group_1[NUM_LEGS / 2] = {1,4,6}; // Divide legs into two self-stable groups
 			uint8_t _leg_group_2[NUM_LEGS / 2] = {2,3,5};
+			double _next_leg_pos[NUM_LEGS][NUM_AXES_PER_LEG];
+			double _leg_X_offset[NUM_LEGS];
+			double _leg_Y_offset[NUM_LEGS];
+			_Bool preCheckSafePos(Position pos)
+			_Bool postCheckSafeCoords(double x, double y, double x)
+
 	};
 
 #endif
