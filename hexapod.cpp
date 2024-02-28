6ea5cf0b04e099639c3db0069173858917c6dfac
zschwid <zschwid@gmail.com>
2024-02-27 21:32:26 -0500

    Update to Dillon's branch

--- hexapod.cpp
+++ hexapod.cpp
@@ -6,13 +6,37 @@
 #include <stdbool.h>
 #include <math.h>
 
-Hexapod::Hexapod() {
-    Leg legs[6]; 
+Hexapod::Hexapod() { 
     for (uint8_t i = 1; i <= NUM_LEGS; i++) {
-        legs[i-1] = Leg(i);
+        legs[i - 1].initializeAxes(i);
     }
 }
 
-_Bool linear_move(Position next_pos) {
-    return true;
+void Hexapod::moveLegToPos(int leg_number, int axis_number, double target_position) {
+	legs[leg_number - 1].axes[axis_number -1].moveToPos(target_position);
+}
+            
+void Hexapod::moveToZeros() {
+	for (uint8_t i = 1; i <= NUM_LEGS; i++) {
+		for (uint8_t j = 1; j <= NUM_AXES_PER_LEG; j++) {
+			legs[i - 1].axes[j - 1].moveToPos(0);	
+		}        
+    }
 } 
+
+void Hexapod::sit() {
+
+}
+            
+void Hexapod::stand() {
+	for (uint8_t i = 1; i <= NUM_LEGS; i++) {
+		legs[i - 1].axes[0].moveToPos(0);
+		legs[i - 1].axes[1].moveToPos(1);
+		legs[i - 1].axes[2].moveToPos(0.75);
+	}
+}
+
+_Bool Hexapod::linearMove(Position next_pos) {
+    return true;
+}
+ 
--- leg.cpp
+++ leg.cpp
@@ -1,20 +1,32 @@
 #include "leg.hpp"
+#include "hexapod.hpp"
 #include "axis.hpp"
+#include "config.hpp"
 #include <stdint.h>
 #include <stdbool.h>
 #include <math.h>
 
-Leg::Leg(uint8_t leg_number) {
+double zero_points[NUM_LEGS][NUM_AXES_PER_LEG] = ZERO_POINTS;
+int pwm_pins[NUM_LEGS][NUM_AXES_PER_LEG] = PWM_PINS;
+double min_pos[NUM_LEGS][NUM_AXES_PER_LEG] = MIN_POS;
+double max_pos[NUM_LEGS][NUM_AXES_PER_LEG] = MAX_POS;
+double scale_fact[NUM_LEGS][NUM_AXES_PER_LEG] = SCALE_FACT;
+
+Leg::Leg() {
+    _leg_number = 0;
+}
+
+void Leg::initializeAxes(uint8_t leg_number) {
     _leg_number = leg_number;
     for (uint8_t i = 0; i < NUM_AXES_PER_LEG; i++) {
-        axes[i] = Axis(pwm_pins[_leg_number-1][i], min_pos[_leg_number-1][i], max_pos[_leg_number-1][i]);
-        axes[i].set_mapping(offsets[_leg_number-1][i], map_mults[_leg_number-1][i]);
+        axes[i].initializePositionLimits(pwm_pins[_leg_number-1][i], min_pos[_leg_number-1][i], max_pos[_leg_number-1][i]);
+        axes[i].setMapping(zero_points[_leg_number-1][i], scale_fact[_leg_number-1][i]);
     }
 }
 
-_Bool Leg::inverse_kinematics(double x, double y, double z) {
+_Bool Leg::inverseKinematics(double x, double y, double z) {
 
-    if (!check_safe_coords(x,y,z))
+    if (!checkSafeCoords(x,y,z))
         return false; 
     double potential_results[3];
     if (fabs(y < 0.0005)) {
@@ -46,41 +58,41 @@ _Bool Leg::inverse_kinematics(double x, double y, double z) {
     // potential_results[1] = theta1_tool0 + theta1_tool1;
     // potential_results[2] = -(PI - theta2_tool);
 
-    for (uint8_t i = 0; i < NUM_AXES; i++) {
+    for (uint8_t i = 0; i < NUM_AXES_PER_LEG; i++) {
         if (potential_results[i] != potential_results[i])  //Check for NaN
             return false;
     }
     
-    for (uint8_t i = 0; i < NUM_AXES; i++) {
+    for (uint8_t i = 0; i < NUM_AXES_PER_LEG; i++) {
         _next_angles[i] = potential_results[i];
     }
     return true;
 }
 
-_Bool Leg::check_safe_coords(double x, double y, double z) {
+_Bool Leg::checkSafeCoords(double x, double y, double z) {
     //If coords within robot body
     //  return false;
     
     return true;
 }
 
-_Bool Leg::rapid_move(double x,  double y, double z) {
-    if (inverse_kinematics(x, y, z)) {
-        move_axes();
+_Bool Leg::rapidMove(double x,  double y, double z) {
+    if (inverseKinematics(x, y, z)) {
+        moveAxes();
         return true;
     }
     return false;
 }
 
-_Bool Leg::linear_move(double x,  double y, double z, double speed) {
+_Bool Leg::linearMove(double x,  double y, double z, double speed) {
     return false;
 }
 
-void Leg::move_axes() {
-    for (uint8_t i = 0; i < NUM_AXES; i++) {
-        axes[i].move_to_pos(_next_angles[i]);
+void Leg::moveAxes() {
+    for (uint8_t i = 0; i < NUM_AXES_PER_LEG; i++) {
+        axes[i].moveToPos(_next_angles[i]);
     }
-    for (uint8_t i = 0; i < NUM_AXES; i++) {
+    for (uint8_t i = 0; i < NUM_AXES_PER_LEG; i++) {
         current_angles[i] = _next_angles[i];
     }
 }
