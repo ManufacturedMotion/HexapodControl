43fd6e3782cf65fe67f742cd47b80c2da6104d5c
Dillon Kane <dkane3@binghamton.edu>
2024-02-28 22:56:43 -0500

    swap defines to nicer format

--- config.hpp
+++ config.hpp
@@ -1,14 +1,9 @@
-#include "axis.hpp"
-
 #ifndef HEXA_CONFIG
 #define HEXA_CONFIG
 
-	#define DANNY 1
-	#define ZACK 2
-	#define DILLON 3
-	#define USER DILLON
+	#define DILLON
 	
-	#if USER == DANNY	
+	#ifdef DANNY	
 
 		#define PWM_PINS {{2, 3, 4}, {5, 6, 7}, {8, 9, 10},  {11, 12, 13}, {14, 15, 18}, {19, 22, 23}}
 		#define ZERO_POINTS {{1.87, 2.30, 2.8}, {2, 2.3, 2.68}, {1.8, 2.2, 2.7}, {2.2, 2.2, 2.5}, {1.93, 2.4, 2.8}, {2.1, 2.1, 2.63}}
@@ -18,7 +13,7 @@
 
 	#endif
 
-	#if USER == ZACK
+	#ifdef ZACK
 
 		#define PWM_PINS {{2, 3, 4}, {5, 6, 7}, {8, 9, 10},  {11, 12, 13}, {14, 15, 18}, {19, 22, 23}}
 		#define ZERO_POINTS {{2.3, 2.6, 2.45}, {2.3, 2.7, 2.05}, {2.85, 2.4, 2.3}, {2.2, 1.85, 2.35}, {2.68, 2.05, 2.3}, {2.25, 2.3, 2.65}}
@@ -28,7 +23,7 @@
 		
 	#endif
 
-	#if USER == DILLON
+	#ifdef DILLON
 
 		#define PWM_PINS {{5, 6, 7}, {2, 3, 4}, {8, 9, 10},  {11, 12, 13}, {14, 15, 18}, {19, 22, 23}}
 		#define ZERO_POINTS {{1.9, 2.45, 2.7}, {2.05, 2.4, 2.7}, {2.35, 2.7, 2.6}, {2.3, 2.4, 2.65}, {2.1, 2.45, 2.65}, {2.35, 2.45, 2.7}}
