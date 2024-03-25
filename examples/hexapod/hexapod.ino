03f5a7350a85ab01e55e2cc967c3b65fa83740bf
Daniel Iacobacci <66583553+Diacoba1@users.noreply.github.com>
2024-03-24 22:58:57 -0400

    update example

--- examples/hexapod/hexapod.ino
+++ examples/hexapod/hexapod.ino
@@ -1,21 +1,9 @@
-//#include "hexapod_controller.hpp"
-
-//Hexapod hexapod; 
-
 void setup() {
-
   Serial.begin(115200);
-  Serial4.begin(115200); //Raspberry Pi Serial
-
+  Serial4.begin(115200); // Raspberry Pi Serial
 }
 
 void loop() {
-  double pos;
-  uint8_t leg_or_action;
-  uint8_t motor;
-  uint8_t speed;
-  hexapod.runSpeed();
-
   if (Serial.available() > 0 || Serial4.available() > 0) {
     String command;
     if (Serial.available() > 0) {
@@ -24,41 +12,54 @@ void loop() {
       command = Serial4.readStringUntil('\n');
     }
 
-    sscanf(command.c_str(), "%hu %hu %lf %hu", &leg_or_action, &motor, &pos, &speed);
-
-    //temp switch for user interaction
-    switch(leg_or_action) {
-      case 1:
-      case 2:
-      case 3:
-      case 4:
-      case 5:
-      case 6:
-        Serial.printf("leg: %d; motor: %d; pos: %f\n", leg_or_action, motor, pos);
-        Serial4.printf("leg: %d; motor: %d; pos: %f\n", leg_or_action, motor, pos);
-        //hexapod.moveLegToPos(leg, motor, pos);
-	hexapod.legs[leg - 1].axes[motor - 1].moveToPosAtSpeed(pos, speed);
-	//hexapod.moveLegToPosAtSpeed(leg, motor, pos, speed); //doesn't exist yet - moveLegToPos uses IK and rapid move func. -- not sure how Zack wanted to combine this with speed
-        break;
-      case 7:
-        Serial.printf("Zeroing all legs\n");
-        Serial4.printf("Zeroing all legs\n");
-        //hexapod.moveToZeros();
-        break;
-      case 8:
-        Serial.printf("Hexapod standing\n");
-        Serial4.printf("Hexapod standing\n");
-        //hexapod.stand();
-        break;
-      case 9:
-        Serial.printf("Hexapod sitting\n");
-        Serial4.printf("Hexapod sitting\n");
-        //hexapod.sit();
-        break;
-      default:
-        Serial.printf("Invalid Input\n");
-        Serial4.printf("Invalid Input\n");
-        break;
+    // Parse command
+    uint8_t leg_or_action;
+    uint8_t motor;
+    double pos;
+    int parsed = sscanf(command.c_str(), "%hhu %hhu %lf", &leg_or_action, &motor, &pos);
+    
+    // Check for valid commands
+    if (parsed == 3 && (leg_or_action >= 1 && leg_or_action <= 6)) {
+      // Process commands requiring three values
+      switch (leg_or_action) {
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+          Serial.printf("leg: %d; motor: %d; pos: %f\n", leg_or_action, motor, pos);
+          Serial4.printf("leg: %d; motor: %d; pos: %f\n", leg_or_action, motor, pos);
+          // Perform action
+          break;
+      }
+    } else if (parsed == 1) {
+      // Process commands requiring only one value
+      switch (leg_or_action) {
+        case 7:
+          Serial.println("Zeroing all legs");
+          Serial4.println("Zeroing all legs");
+          // Perform action
+          break;
+        case 8:
+          Serial.println("Hexapod standing");
+          Serial4.println("Hexapod standing");
+          // Perform action
+          break;
+        case 9:
+          Serial.println("Hexapod sitting");
+          Serial4.println("Hexapod sitting");
+          // Perform action
+          break;
+        default:
+          Serial.println("Invalid Input");
+          Serial4.println("Invalid Input");
+          break;
+      }
+    } else {
+      // Invalid command format
+      Serial.println("Invalid command format");
+      Serial4.println("Invalid command format");
     }
-	}
+  }
 }
