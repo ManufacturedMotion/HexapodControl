033f92d8563a533ea84a2a4986e0c0310e265fb6
Dillon Kane <dkane3@binghamton.edu>
2024-04-14 10:19:34 -0400

    working fifo

--- FIFOCommandQueue.cpp
+++ FIFOCommandQueue.cpp
@@ -7,19 +7,21 @@ void FIFOCommandQueue::enqueue(String str_command) {
 	Command* new_command = new Command(str_command);
 	if (head == NULL) {
         head = new_command;
+		head->next = NULL;
     }
     else {
         tail->next = new_command;
     }
     tail = new_command;
+	tail->next = NULL;
 	length++;
 }
 
 String FIFOCommandQueue::dequeue() {
-    if (head != NULL) {
+	if (head != NULL) {
         String ret_string = head->command;
         Command* temp = head;
-		head = head->next;
+		head = temp->next;
 		delete temp;
 		length--;
         return ret_string;
