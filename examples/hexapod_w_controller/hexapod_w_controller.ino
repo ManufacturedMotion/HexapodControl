787c6d9c16b6415b8fe155157a7019dc8353d513
Dillon Kane <dkane3@binghamton.edu>
2024-04-14 00:49:07 -0400

    fixes

--- examples/hexapod_w_controller/hexapod_w_controller.ino
+++ examples/hexapod_w_controller/hexapod_w_controller.ino
@@ -95,7 +95,7 @@ void loop() {
 			if (split_command[0].startsWith('g')) {
 
 				splitString(split_command[0], 'G', buffer, num_words);
-				if(!buffer[1].equals("0") or !buffer[1].equals("1")){
+				if(!buffer[1].equals("0") and !buffer[1].equals("1")){
 					Serial.println("Error: only G0 and G1 implemented");
 					Serial4.println("Error: only G0 and G1 implemented");
 				}
@@ -135,14 +135,6 @@ void loop() {
 	
       				}		
 		
-				}
-			}
-		
-			else if (split_command[0].startsWith('p')) {
-				splitString(split_command[0], 'P', buffer, num_words);	
-				if (buffer[1] == "0") {
-			
-					Serial.println("parsing success; starfish preset selected (move all motors to zero)\n");
 				}
 			}
 		
