fe383a5367bb01eb77d57bf8aad28aeb912804d5
Daniel Iacobacci <66583553+Diacoba1@users.noreply.github.com>
2024-05-30 21:06:18 -0400

    Update config.hpp

--- config.hpp
+++ config.hpp
@@ -5,12 +5,12 @@
 #define HEXA_CONFIG
 
 	#define DEBUG true
-	#define ZACK
+	#define DUMMY_USER
 
 	
 	#ifdef DANNY	
 		#define PWM_PINS {{5, 6, 7}, {2, 3, 4}, {8, 9, 10},  {11, 12, 13}, {14, 15, 18}, {19, 22, 23}}
-		#define ZERO_POINTS {{2, 2.3, 2.68}, {1.87, 2.30, 2.8}, {1.8, 2.2, 2.7}, {2.2, 2.2, 2.5}, {1.93, 2.4, 2.8}, {2.1, 2.1, 2.63}}
+		#define ZERO_POINTS {{2.05, 2.3, 2.68}, {1.87, 2.30, 2.8}, {1.8, 2.2, 2.65}, {2.2, 2.15, 2.5}, {2.2, 2.4, 2.8}, {2.15, 2.35, 2.53}}
 		#define MAX_POS {{PI, PI, PI}, {PI, PI, PI}, {PI, PI, PI}, {PI, PI, PI}, {PI, PI, PI}, {PI, PI, PI}}
 		#define MIN_POS {{-PI, -PI, -PI}, {-PI, -PI, -PI}, {-PI, -PI, -PI}, {-PI, -PI, -PI}, {-PI, -PI, -PI}, {-PI, -PI, -PI}}
 		#define SCALE_FACT {{0.75, 0.75, 0.75}, {0.75, 0.75, 0.75}, {0.75, 0.75, 0.75}, {0.75, 0.75, 0.75}, {0.75, 0.75, 0.75}, {0.75, 0.75, 0.75}}
